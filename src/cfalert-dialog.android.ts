c05053f491a75b07353e3ebc1f52d3390692b12d
Shiv19 <mailshiva19@gmail.com>
2017-10-25 21:45:53 +0530

    updated android.ts

--- src/cfalert-dialog.android.ts
+++ src/cfalert-dialog.android.ts
@@ -19,43 +19,44 @@ export enum CFAlertActionAlignment {
     JUSTIFIED = 3
 }
 
-declare module com {
-    export module crowdfire {
-        export module cfalertdialog {
-            export class CFAlertDialog extends android.support.v7.app.AppCompatDialog {
-                public show():void;
-                public dismiss():void;
-                public setBackgroundColor(color: any, animated: boolean):void;
-                public setDialogBackgroundColor(color: any, animated: boolean):void;
-                public setCornerRadius(radius: number):void;
-                public setTitle(title: any):void;
-                public setTitleColor(color: any):void;
-                public setMessage(message: string):void;
-                public setMessageColor(color: number):void;
-                public setDialogStyle(dialogStyle: CFAlertStyle):void;
-                public setTextGravity(textGravity: number):void;
-                public setHeaderView(view: android.view.View):void;
-                public setIcon(drawable: any):void;
-                public setContentImageDrawable(drawable: any):void;
-                public setFooterView(view: android.view.View):void;
-                public setItems(items: string[], OnClickListener: any):void;
-                public setMultiSelectItems(multiSelectItems: string[], selectedItems: boolean[], OnMultiChoiceClickListener: any):void;
-                public setSingleSelectItems(singleSelectItems: string[], selectedItem: number, OnClickListener: any):void;
-                public setElevation(elevation: number);
-                public static Builder;
-            }
-        }
-    }
-}
+// declare module com {
+//     export module crowdfire {
+//         export module cfalertdialog {
+//             export class CFAlertDialog extends android.support.v7.app.AppCompatDialog {
+//                 public show():void;
+//                 public dismiss():void;
+//                 public setBackgroundColor(color: any, animated: boolean):void;
+//                 public setDialogBackgroundColor(color: any, animated: boolean):void;
+//                 public setCornerRadius(radius: number):void;
+//                 public setTitle(title: any):void;
+//                 public setTitleColor(color: any):void;
+//                 public setMessage(message: string):void;
+//                 public setMessageColor(color: number):void;
+//                 public setDialogStyle(dialogStyle: CFAlertStyle):void;
+//                 public setTextGravity(textGravity: number):void;
+//                 public setHeaderView(view: android.view.View):void;
+//                 public setIcon(drawable: any):void;
+//                 public setContentImageDrawable(drawable: any):void;
+//                 public setFooterView(view: android.view.View):void;
+//                 public setItems(items: string[], OnClickListener: any):void;
+//                 public setMultiSelectItems(multiSelectItems: string[], selectedItems: boolean[], OnMultiChoiceClickListener: any):void;
+//                 public setSingleSelectItems(singleSelectItems: string[], selectedItem: number, OnClickListener: any):void;
+//                 public setElevation(elevation: number);
+//                 public static Builder;
+//             }
+//         }
+//     }
+// }
 
-var CFAD = com.crowdfire.cfalertdialog.CFAlertDialog;
+// var CFAD = com.crowdfire.cfalertdialog.CFAlertDialog;
+declare var com: any;
 export class CFAlertDialog {
     public show(options) {
         var _options = options || {};
         
         return new Promise<{}>((resolve, reject) => {
             // try{
-                var builder = new CFAD.Builder(app.android.context)
+                var builder = new com.crowdfire.cfalertdialog.CFAlertDialog.Builder(app.android.foregroundActivity)
                     .setDialogStyle(CFAlertStyle.ALERT)
                     .setTitle("Hello World!")
                     .setMessage("Alert worked!")
