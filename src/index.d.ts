91ee7229c48424009c5ba160f25be3a0c7e76d83
Shiva Prasad <mailshiva19@gmail.com>
2017-12-18 16:11:47 +0530

    property indentation

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,18 @@
 export declare enum CFAlertStyle {
     NOTIFICATION = 0,
     ALERT = 1,
-    BOTTOM_SHEET = 2,
+    BOTTOM_SHEET = 2
 }
 export declare enum CFAlertActionStyle {
     DEFAULT = 0,
     NEGATIVE = 1,
-    POSITIVE = 2,
+    POSITIVE = 2
 }
 export declare enum CFAlertActionAlignment {
     START = 0,
     END = 1,
     CENTER = 2,
-    JUSTIFIED = 3,
+    JUSTIFIED = 3
 }
 export enum CFAlertGravity {
     START = 0,
@@ -23,39 +23,41 @@ export enum CFAlertGravity {
 export interface DialogOptions {
     dialogStyle: CFAlertStyle;
     title: string;
-    titleColor?: string,
+    titleColor?: string;
     message?: string;
     messageColor?: string;
     textColor?: string;
     textAlignment?: CFAlertGravity;
-    backgroundColor?: string,
-    backgroundBlur?: boolean,
-    cancellable?: boolean,
-    headerView?: any, // nativeView
-    footerView?: any, // nativeView
-    onDismiss?: Function, // calback for dismiss
-    buttons?: [{
-        text: string, // title
-        buttonStyle: CFAlertActionStyle,
-        buttonAlignment?: CFAlertActionAlignment,
-        textColor?: string,
-        backgroundColor?: string,
-        onClick: Function
-    }],    
+    backgroundColor?: string;
+    backgroundBlur?: boolean;
+    cancellable?: boolean;
+    headerView?: any; // nativeView
+    footerView?: any; // nativeView
+    onDismiss?: Function; // calback for dismiss
+    buttons?: [
+        {
+            text: string; // title
+            buttonStyle: CFAlertActionStyle;
+            buttonAlignment?: CFAlertActionAlignment;
+            textColor?: string;
+            backgroundColor?: string;
+            onClick: Function;
+        }
+    ];
     simpleList?: {
-        items: [string],
-        onClick: Function
-    },
+        items: [string];
+        onClick: Function;
+    };
     singleChoiceList?: {
-        items: [string],
-        selectedItem: number,
-        onClick: Function
-    },
+        items: [string];
+        selectedItem: number;
+        onClick: Function;
+    };
     multiChoiceList?: {
-        items: [string],
-        selectedItems: [boolean],
-        onClick: Function
-    }
+        items: [string];
+        selectedItems: [boolean];
+        onClick: Function;
+    };
 }
 export declare class CFAlertDialog {
     show(options: any): void;
